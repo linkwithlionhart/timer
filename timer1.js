dc9f47ad8f65ccb5dcbf60410c704634b8a6da52
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-02 08:39:11 -0700

    Complete challenge for edge cases and output within expectations

--- timer1.js
+++ timer1.js
@@ -1,17 +1,19 @@
-// Process command line arguments, skipping the first two elements in the string array
+// Process the command line arguments, skipping the first two elements in the string array
 const CLargs = process.argv.slice(2);
 
+// Using 'method chaining' on the command line arguments variable
+// Using arrow functions for brevity
 CLargs 
-  // Convert string numbers to numbers
+  // Convert the string command line arguments to numbers
   .map(Number)
   // Filter out any non-numbers or negative numbers
   .filter(num => !isNaN(num) && num >= 0)
-  // Sort the numbers in ascending order
+  // Sorting the numbers in ascending order
   .sort((a,b) => a - b) 
   // For each number, set a timeout to beep after the number of seconds
   .forEach((num, i) => {
     setTimeout(() => {
       process.stdout.write('\x07');
-      console.log(`Beep at ${num} second${num === 1 ? '' : 's'}`);
-    }, num * 1000); // Convert s to ms
+      console.log(`Beep at ${num} second${num === 1 ? '' : 's'}`); // Using ternary operator to apply second or seconds depending if more than 1
+    }, num * 1000);
   });
